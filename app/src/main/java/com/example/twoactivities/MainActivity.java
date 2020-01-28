f18751e9bd1ed12dbbb89f064f47034fed0d2ec0
Rafael Menchaca <rafaelmenchaca24@gmail.com>
2020-01-27 23:18:06 -0600

    Added Log statements

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -24,7 +24,46 @@ public class MainActivity extends AppCompatActivity {
         mMessageEditText = findViewById(R.id.editText_main); // References the EditText
         mReplyHeadTextView = findViewById(R.id.text_header_reply); // References the reply header
         mReplyTextView = findViewById(R.id.text_message_reply); // References the reply message
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate"); // Log the start of the onCreate() method.
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,6 +12,7 @@ import android.widget.TextView;
 public class SecondActivity extends AppCompatActivity {
     public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"; // Key
     private EditText mReply; //The variable holds the EditText
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +26,48 @@ public class SecondActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
     public void returnReply(View view) {
         String reply = mReply.getText().toString(); // Gets the text of the EditText as a string
         Intent replyIntent = new Intent(); // A new intent for the response
         replyIntent.putExtra(EXTRA_REPLY, reply); // The reply string from the EditText is added to the new intent as an Intent extra. Extras are key/value pairs, here the key is EXTRA_REPLY, and the value is the reply.
         setResult(RESULT_OK,replyIntent); // Sets the result to RESULT_OK to indicate that the response was successful. The Activity class defines the result codes, including RESULT_OK and RESULT_CANCELLED.
+        Log.d(LOG_TAG, "End SecondActivity");
         finish(); // Closes the Activity and returns to MainActivity.
 
     }
